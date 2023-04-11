778718bb53bd6c9cdd6ee6ed37c0ea1c9295be2c
IsaiasC1999 <100697426+IsaiasC1999@users.noreply.github.com>
2023-04-11 12:47:35 -0300

    app update

--- src/App.jsx
+++ src/App.jsx
@@ -16,9 +16,9 @@ function App() {
       <Header />
       <div className="app-conteiner">
         <Main>
-          {/* Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, sit praesentium? Velit magnam vero vitae quidem voluptatum mollitia voluptate nesciunt modi earum omnis repellendus, perspiciatis doloremque eius iusto. Beatae, cumque. */}
-          {/* <Home /> */}
-          <Login></Login>
+          
+          <Home />
+          {/* <Login></Login> */}
         </Main>
         <Footer />
       </div>
